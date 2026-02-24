80336d9a30008af39f223fd0528fb3904f109490
Koutakis <alexander.koutakis00@gamil.com>
2026-02-24 11:16:58 +0100

    postgres connections and DSN support from roskarl

--- config/connections.py
+++ config/connections.py
@@ -1,11 +1,15 @@
 import pyodbc
 import psycopg
-from roskarl import env_var_dsn
+from roskarl import env_var_dsn, DSN
 
 
-def _build_mssql_conn(source_dns: str) -> str:
-    dsn = env_var_dsn(name=source_dns)
-    return (
+def get_postgres_connection(dsn: DSN) -> psycopg.Connection:
+    conn_string = f"host={dsn.hostname} port={dsn.port} dbname={dsn.database} user={dsn.username} password={dsn.password}"
+    return psycopg.connect(conn_string)
+
+
+def get_mssql_connection(dsn: DSN, timeout: int = 600) -> pyodbc.Connection:
+    conn_string = (
         f"DRIVER={{ODBC Driver 18 for SQL Server}};"
         f"SERVER={dsn.hostname},{dsn.port};"
         f"DATABASE={dsn.database};"
@@ -13,20 +17,9 @@ def _build_mssql_conn(source_dns: str) -> str:
         f"PWD={dsn.password};"
         f"TrustServerCertificate=yes"
     )
-
-
-def _build_postgres_conn(dest_env: str) -> str:
-    dsn = env_var_dsn(name=dest_env)
-    return f"host={dsn.hostname} port={dsn.port} dbname={dsn.database} user={dsn.username} password={dsn.password}"
-
-
-def get_mssql_connection(source_dns: str, timeout: int = 600) -> pyodbc.Connection:
-    conn_string = _build_mssql_conn(source_dns)
     conn = pyodbc.connect(conn_string, timeout=timeout)
     conn.timeout = timeout
     return conn
 
 
-def get_postgres_connection(dest_env: str) -> psycopg.Connection:
-    conn_string = _build_postgres_conn(dest_env)
-    return psycopg.connect(conn_string)
+
