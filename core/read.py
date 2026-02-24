222f56d84a988f4fe263a68988e8f5755c3897b9
Koutakis <alexander.koutakis00@gamil.com>
2026-02-24 11:19:04 +0100

    speeeeeed improvement, only use batching when needed

--- core/read.py
+++ core/read.py
@@ -1,21 +1,29 @@
 from collections.abc import Generator
 import polars as pl
 from config.connections import get_mssql_connection
-from roskarl import env_var_dsn, DSN
+from roskarl import env_var_dsn
 
 
-def read(source_env: str, query: str, batch_size: int = 100_000) -> Generator[pl.DataFrame, None, None]:
-    dsn: DSN = env_var_dsn(name=source_env)
-    conn = get_mssql_connection(source_env)
-    cursor = conn.cursor()
-    cursor.execute(query)
-    columns = [desc[0] for desc in cursor.description]
+def read(env_name: str, query: str, batch_size: int | None = None) -> Generator[pl.DataFrame, None, None]:
+    dsn = env_var_dsn(name=env_name)
+    conn = get_mssql_connection(dsn)
 
-    while True:
-        rows = cursor.fetchmany(batch_size)
-        if not rows:
-            break
-        yield pl.DataFrame([dict(zip(columns, row)) for row in rows])
+    if batch_size is None:
+        df = pl.read_database(query, conn)
+        conn.close()
+        yield df
+    else:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        columns = [desc[0] for desc in cursor.description]
 
-    cursor.close()
-    conn.close()
+        while True:
+            rows = cursor.fetchmany(batch_size)
+            if not rows:
+                break
+            yield pl.DataFrame(
+                {col: [row[i] for row in rows] for i, col in enumerate(columns)}
+            )
+
+        cursor.close()
+        conn.close()
